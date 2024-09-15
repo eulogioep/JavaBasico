fe1eb0960dc9d2016531de34a12193a51236eab2
Eulogio Enamorado Pallares <eulogio-ep@hotmail.com>
2024-09-15 20:07:56 +0200

    Condicional if - else

---
+++ JavaSEBasico/src/Teoria/CondicionalIF.java
@@ -0,0 +1,17 @@
+package Teoria;
+
+public class CondicionalIF {
+    public static void main(String[] args) {
+        System.out.println("\n--- Estructura Condicional: if-else ---");
+
+        int numero = 15;
+
+        if (numero > 10) {
+            System.out.println("El número es mayor que 10");
+        } else if (numero < 10) {
+            System.out.println("El número es menor que 10");
+        } else {
+            System.out.println("El número es igual a 10");
+        }
+    }
+}
